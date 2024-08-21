cc39fd3eaa758a4b363e0c3f25c69abf44c5bcdd
vintcessun <vintces@163.com>
2024-08-21 23:11:43 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,32 @@
-mod sql;
 mod get_video_list;
-use get_video_list::Video;
+pub mod sql;
 use anyhow::Result;
-pub use get_video_list::Videoplay;
+pub use get_video_list::*;
 
 #[derive(Debug)]
-pub struct Videos{
-    videos: Vec<Video>
+pub struct Videos {
+    videos: Vec<Video>,
 }
 
-impl Videos{
-    pub fn get()->Result<Self>{
+impl Videos {
+    pub fn get() -> Result<Self> {
         let ret = sql::get()?;
         let videos = get_video_list::resort(ret);
-        Ok(Self{videos})
+        Ok(Self { videos })
     }
 
-    pub fn random(&self)->Result<Vec<Videoplay>>{
+    pub fn random(&self) -> Result<Vec<Videoplay>> {
         get_video_list::get_random_url_list(&self.videos)
     }
 
-    pub fn index(&self, index: usize)->Video{
+    pub fn index(&self, index: usize) -> Video {
         self.videos[index].clone()
     }
 
-    pub fn renew(&mut self)->Result<()>{
+    pub fn renew(&mut self) -> Result<()> {
         let ret = sql::get()?;
         let videos = get_video_list::resort(ret);
-        *self = Self{videos};
+        *self = Self { videos };
         Ok(())
     }
 }
-
-
-
